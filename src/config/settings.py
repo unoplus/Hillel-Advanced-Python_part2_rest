83bb0ca294b11248fa8cbf223bc724430d65df15
unoplus <unoplus6t@gmail.com>
2022-02-05 13:28:09 -0800

    adding connection to postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "rest_posts",
+        "USER": "postgres",
+        "PASSWORD": "Fm#1bpO40c+$r",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
