145f7bdcb9e93e3fbc11c980ee1f9480ccba18ae
unoplus <unoplus6t@gmail.com>
2022-02-14 06:20:28 -0800

    admin panel

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+
 from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at', 'updated_at', )
-    search_fields = ('title', )
-    list_filter = ('author', 'updated_at')
-    ordering = ('-updated_at',)
+    list_display = (
+        "title",
+        "author",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("title",)
+    list_filter = ("author", "updated_at")
+    ordering = ("-updated_at",)
 
 
 admin.site.register(Post, PostAdmin)
