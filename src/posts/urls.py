abd7821c00f7242a072c914aa840bdbfe7545364
unoplus <unoplus6t@gmail.com>
2022-02-23 13:51:24 -0800

    some routing changes

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .api import PostListCreateAPI, PostRetrieveUpdateDestroyAPI
+from posts.api import PostListCreateAPI, PostRetrieveUpdateDestroyAPI
 
 app_name = "posts"
 
 urlpatterns = [
-    path("", PostListCreateAPI.as_view(), name="list"),
+    path("posts/", PostListCreateAPI.as_view(), name="list"),
     path("<int:id>/", PostRetrieveUpdateDestroyAPI.as_view(), name="retrieve"),
+    # path("create/", PostCreateAPI.as_view(), name="create"),
 ]
