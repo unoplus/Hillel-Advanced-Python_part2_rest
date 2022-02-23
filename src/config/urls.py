81a1d008fd64298cee0962e1e71e44beaf9c6ee7
unoplus <unoplus6t@gmail.com>
2022-02-23 13:51:11 -0800

    some routing changes

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,15 +1,34 @@
+from time import sleep
+
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
+from config.celery import app, health_check
+from posts.models import HealthCheck
+
+
+@app.task
+def update_counter():
+    sleep(3)
+    instance = HealthCheck.objects.first()
+    instance.counter += 1
+    instance.save()
+    print(f"Counter was updated: {instance.counter}")
+
 
 def check(request):
     data = {"Hello": "World!"}
+
+    update_counter.delay()
+
+    health_check.delay()
+
     return JsonResponse(data)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health-check/", check),
-    path("posts/", include("posts.urls")),
+    path("", include("posts.urls")),
 ]
