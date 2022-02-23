347772afc7aec9057839de9addba65593ce64b43
unoplus <87710423+unoplus@users.noreply.github.com>
2022-02-24 00:11:20 +0200

    Update urls.py

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,5 +7,4 @@ app_name = "posts"
 urlpatterns = [
     path("posts/", PostListCreateAPI.as_view(), name="list"),
     path("<int:id>/", PostRetrieveUpdateDestroyAPI.as_view(), name="retrieve"),
-    # path("create/", PostCreateAPI.as_view(), name="create"),
 ]
